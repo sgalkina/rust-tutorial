7b81e4d1328f07933de3974c19785d327a33d54e
Svetlana Kutuzova <galkina.s.a@gmail.com>
2022-10-06 20:04:27 +0200

    Format

--- hello.rs
+++ hello.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::fmt::{self, Formatter, Display, UpperHex};
 
 struct Structure(i32);
 
@@ -23,9 +23,78 @@ impl fmt::Display for Complex {
     }
 }
 
+///////////
+
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Extract the value using tuple indexing,
+        // and create a reference to `vec`.
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // Iterate over `v` in `vec` while enumerating the iteration
+        // count in `count`.
+        for (count, v) in vec.iter().enumerate() {
+            // For every element except the first, add a comma.
+            // Use the ? operator to return on errors.
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}: {}", count, v)?;
+        }
+
+        // Close the opened bracket and return a fmt::Result value.
+        write!(f, "]")
+    }
+}
+
+///////////
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl UpperHex for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = format!("{:X}{:X}{:X}", self.red, self.green, self.blue);
+        write!(f, "{:0>6}", s)
+    }
+}
+
+impl Display for Color {
+    // `f` is a buffer, and this method must write the formatted string into it
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "RGB ({}, {}, {}) 0x{:X}", self.red, self.green, self.blue, self)
+    }
+}
+
+// RGB (128, 255, 90) 0x80FF5A
+// RGB (0, 3, 254) 0x0003FE
+// RGB (0, 0, 0) 0x000000
+
 fn main() {
     let complex = Complex{real: 3.3, imag: 7.2};
     println!("Compare Complex");
     println!("Display: {}", complex);
     println!("Debug: {:?}", complex);
+
+    ///////////
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+
+    ///////////
+
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        println!("{}", *color);
+    }
 }
